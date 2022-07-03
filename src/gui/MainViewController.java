3940cb8ca18b6141152248d7f553ea48911c7ac2
Victor <jv1555550@gmail.com>
2022-07-02 20:40:30 -0500

    Main View Controller

---
+++ src/gui/MainViewController.java
@@ -0,0 +1,46 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+
+public class MainViewController implements Initializable{
+
+	@FXML
+	private MenuItem MenuDepartment;
+	
+	@FXML
+	private MenuItem MenuSeller;
+	
+	@FXML
+	private MenuItem menuAbout;
+	
+	@FXML
+	public void actionmenudepartment() {
+		
+	}
+	
+	@FXML
+	public void actionmenuseller() {
+		
+	}
+	
+	@FXML
+	public void actionabout() {
+		
+	}
+	
+	
+	
+	
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
